7b5ffe797d9a01796957b58411e8ffd0c0e8d26e
JaquelineVictal <jaquelinevictal@gmail,com>
2022-08-15 20:54:06 -0300

    Feature Edit Event

--- js/admin.js
+++ js/admin.js
@@ -1,4 +1,5 @@
 
+import { EdicaoEvent } from "./editar-evento";
 
 const tableListEvent = document.getElementById('table');
 
@@ -6,10 +7,11 @@ tableListEvent.addEventListener('click', (event) =>{
         const button= event.path[0].innerText
         const id =  event.path[0].id
         
-        if(button == 'Editar'){
-            PutEventById(id)
+        if(button == 'editar'){
+            EdicaoEvento(id)
+                        
         }
-        if(button == 'Excluir'){
+        if(button == 'excluir'){
             deletarEvento(id)
         }
         if(button == 'ver reservas'){
--- js/editar-evento.js
+++ js/editar-evento.js
@@ -1,31 +1,47 @@
 const SOUND_URL = "https://xp41-soundgarden-api.herokuapp.com"
 
-const FormEditEvent = document.getElementById('EditEvent');
+const EdicaoEvento = (id) => {
+    console.log(id)
 
-FormEditEvent.addEventListener('submit', (event) =>{
+    // Obtendo as informações do evento pelo metodo GET
 
-    const inputNome = document.getElementById("nome")
-    const inputBanner = document.getElementById("banner")
-    const inputAtracoes = document.getElementById("atracoes")
-    const inputDescricao = document.getElementById("descricao")
-    const inputData = document.getElementById("data")
-    const inputLotacao = document.getElementById("lotacao")
+    getEvent().then((dados) => {
+        dados.forEach(element => {
+            if(element.id == id){
+                AdicionaParametroNoInput(element);
+            }
+            
+        });
+    }) 
 
-    const fullDateTime = new Date(inputData.value)
+    const FormEditEvent = document.getElementById('EditEvent');
 
-   const EditEvent = {
-    "name": inputNome.value,
-    "poster": inputBanner.value,
-    "attractions": inputAtracoes.value.split(","),
-    "description": inputDescricao.value,
-    "scheduled": fullDateTime,
-    "number_tickets": inputLotacao.value
-   }
+    FormEditEvent.addEventListener('submit', (event) =>{
 
-   PutEventById(EditEvent,id);
+        const inputNome = document.getElementById("nome")
+        const inputBanner = document.getElementById("banner")
+        const inputAtracoes = document.getElementById("atracoes")
+        const inputDescricao = document.getElementById("descricao")
+        const inputData = document.getElementById("data")
+        const inputLotacao = document.getElementById("lotacao")
+
+        const fullDateTime = new Date(inputData.value)
+
+    const EditEvent = {
+        "name": inputNome.value,
+        "poster": inputBanner.value,
+        "attractions": inputAtracoes.value.split(","),
+        "description": inputDescricao.value,
+        "scheduled": fullDateTime,
+        "number_tickets": inputLotacao.value
+    }
+
+    PutEventById(EditEvent,id);
 
 
-})
+    })
+
+}
 
 
 const PutEventById = (EditEvent,id) => {
@@ -44,3 +60,30 @@ const PutEventById = (EditEvent,id) => {
     })
     .catch(error => console.error(error))
 };
+
+
+const getEvent = ()  => {
+    return fetch(SOUND_URL).then((response) => {
+        if(response.status != 200){
+            console.log("Erro no servidor: ${response.status}")
+        }
+        else {
+            return response.json()
+        }
+    });
+};
+
+const AdicionaParametroNoInput = (objeto) => {
+
+    const FormEditEvent = document.getElementById('EditEvent');
+
+    
+    FormEditEvent.getElementById('nome').value = objeto.nome
+    FormEditEvent.getElementById('banner').value = objeto.banner
+    FormEditEvent.getElementById('atracoes').value = objeto.atracoes
+    FormEditEvent.getElementById('descricao').value =  objeto.descricao
+    FormEditEvent.getElementById('data').value = objeto.data
+    FormEditEvent.getElementById('lotacao').value =  objeto.lotacao
+
+}
+
