45b45eb7e95d10accde97c81373a4e0a602230c7
MonicaIacy <monicamendes81@yahoo.com>
2022-08-17 18:52:14 -0300

    fix href reservas-evento.html

--- js/admin.js
+++ js/admin.js
@@ -1,33 +1,45 @@
-const SOUND_URL = "https://xp41-soundgarden-api.herokuapp.com/events"
 
-const eventsList =  async () => {
-  
-  const table = document.querySelector("tbody")
-  
-  const events = await fetch(`${SOUND_URL}`, {
-    method: "GET",
-    redirect: "follow",
-    headers: {"Content-Type": "application/json"}
-  }).then((response) => {
-    return response.json()
-  })
-  
+const SOUND_URL = 'https://xp41-soundgarden-api.herokuapp.com/events';
 
-  events.forEach((item) => {
-    table.innerHTML += `
-    <tr>
-      <th scope="row">${events.indexOf(item) + 1}</th>
-      <td>${item.scheduled}<td>
-      <td>${item.name}</td>
-      <td>${item.attractions}</td>
-      <td>
-        <a href="reservas-evento.html?id=${item._id}" class="btn btn-dark">ver reservas</a>
-        <a href="editar-evento.html?id=${item._id}" class="btn btn-secondary">editar</a>
-        <a href="excluir-evento.html?id=${item._id}" class="btn btn-danger">excluir</a>
-      </td>
-    </tr>
-    `
-  })
+const listarEventos = async () => {
+
+    const eventos = await fetch(SOUND_URL, {
+        method: "GET",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then((resposta) => {
+
+        //retorna lista em array de objetos
+        return resposta.json();
+    });
+
+    // console.log(eventos);
+
+    const tbody = document.querySelector('.lista-eventos tbody');
+
+    let htmlEventos = "";
+
+    eventos.forEach(evento => {
+        htmlEventos += `
+            <tr>
+                <th scope="row">#</th>
+                <td>${evento.scheduled}</td>
+                <td>${evento.name}</td>
+                <td>${evento.attractions.join(', ')}</td>
+                <td>
+                  <a href="reservas-evento.html?id=${evento._id}" class="btn btn-dark">ver reservas</a>
+                  <a href="editar-evento.html?id=${evento._id}" class="btn btn-secondary">editar</a>
+                  <a href="excluir-evento.html?id=${evento._id}" class="btn btn-danger">excluir</a>
+                </td>
+              </tr>
+        `;
+    });
+
+    tbody.innerHTML = htmlEventos;
+
+
 }
 
-eventsList()
+listarEventos();
